93a72396899da1c66a8e475430a860ebf3c3025e
ecehavanci <ecehavanci@gmail.com>
2025-06-02 20:27:32 +0300

    Updates

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="bg-black text-gray-400 text-center py-6 text-sm">
-            <p>
-                © {new Date().getFullYear()} Ece Havancı. Tüm hakları saklıdır.
-            </p>
-        </footer>
-    );
-}
